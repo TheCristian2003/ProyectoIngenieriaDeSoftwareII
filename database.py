7d014932c071819e0923c07a8f8954e3e359707f
TheCristian2003 <cristianmilian12345@gmail.com>
2025-11-06 02:38:29 -0500

    variables de entorno de la conexion a base de datos actualizadas

--- database.py
+++ database.py
@@ -4,18 +4,17 @@ from mysql.connector import Error
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host='localhost',
+            host='mainline.proxy.rlwy.net',
             user='root',
-            password='0000',
+            password='RIhbOcIqREumPdCoPuYuATjbQAHyWxhi',
             database='tienda_tecnologia',
-            port=3306
+            port=40033
         )
         print("✅ Conexión a MySQL exitosa")
         return connection
     except Error as e:
         print(f"❌ Error conectando a MySQL: {e}")
         return None
-
 # Función para agregar datos de prueba (opcional)
 def agregar_datos_prueba():
     conn = get_db_connection()

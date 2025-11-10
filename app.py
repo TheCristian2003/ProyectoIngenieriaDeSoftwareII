dc96e12776a39b12540e7b0a0b4f9fefcfc23060
TheCristian2003 <cristianmilian12345@gmail.com>
2025-11-10 16:04:10 -0500

    comando para alojar en render

--- app.py
+++ app.py
@@ -705,4 +705,6 @@ def api_detalle_carrito_db():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
